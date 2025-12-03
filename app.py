cd0e911fd20ecbd58f217be1dd869a8438aef745
malakfouad8888 <malakfouad8888@gmail.com>
2025-12-03 22:46:12 +0100

    Deploy PythonAnywhere version

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def add_quote():
         save_quotes(quotes)
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
